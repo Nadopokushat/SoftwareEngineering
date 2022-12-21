bed5770c3532e743e35323f54bddc1ebbf7bead0
svsh195 <svsh195@gmail.com>
2022-12-21 22:27:46 +0700

    Lab3

---
+++ Lab3/Triangle/RightTriangle.cs
@@ -0,0 +1,11 @@
+﻿namespace Triangle;
+
+class RightTriangle : Triangle
+{
+    public RightTriangle(double legA, double legB): base(legA, legB, Math.Sqrt(Math.Pow(legA, 2) + Math.Pow(legB, 2))) { }
+
+    public override double GetPerimeter()
+    {
+        return sideA + sideB + sideC;
+    }
+}
---
+++ Lab3/Triangle/Triangle.cs
@@ -0,0 +1,26 @@
+﻿namespace Triangle;
+
+abstract class Triangle
+{
+    private protected double sideA;
+    private protected double sideB;
+    private protected double sideC;
+
+    public Triangle(double sideA, double sideB, double sideC)
+    {
+        if ((sideA <= 0) || (sideB <= 0) || (sideC <= 0))
+        {
+            throw new ArgumentException("The side of triangle cannot be 0 or be a negative number.");
+        }
+        else if ((sideA + sideB <= sideC) || (sideA + sideC <= sideB) || (sideB + sideC <= sideA))
+        {
+            throw new ArgumentException("Any two sides of a triangle cannot be less than or equal to the third side.");
+        }
+
+        this.sideA = sideA;
+        this.sideB = sideB;
+        this.sideC = sideC;
+    }
+
+    public abstract double GetPerimeter();
+}
