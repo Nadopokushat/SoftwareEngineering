ce889b1f0d64cb14f9fad3448b7014e0cc93bc05
svsh195 <svsh195@gmail.com>
2023-01-08 21:05:48 +0700

    Add #region in Lab1

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,2 +1,12 @@
-﻿static double X(double a) => 12 * Math.Pow(a, 2) + 7 * a - 16;
+﻿#region Task1
+static double X(double a) => 12 * Math.Pow(a, 2) + 7 * a - 16;
 static double Y(double x) => 7 * Math.Pow(x, 2) - 3 * x + 6;
+#endregion
+
+#region Task2
+// Quadrilateral
+#endregion
+
+#region Task3
+// Temperature
+#endregion
