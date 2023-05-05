674190bc4de29eea11c5f7f0e2711c08e8ba3130
Sviatoslav <svsh195@gmail.com>
2023-05-05 14:32:35 +0700

    Lab10

---
+++ Lab10/Box.cs
@@ -0,0 +1,44 @@
+﻿class Box
+{
+    private double length;
+    private double width;
+    private double height;
+    private double weight;
+
+    public Box() : this(Random.Shared.NextDouble() * 10, Random.Shared.NextDouble() * 10, Random.Shared.NextDouble() * 10, Random.Shared.NextDouble() * 10)
+    {
+    }
+
+    public Box(double length, double width, double height, double weight)
+    {
+        this.length = length;
+        this.width = width;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    public double Length
+    {
+        get { return length; }
+    }
+
+    public double Width
+    {
+        get { return width; }
+    }
+
+    public double Height
+    {
+        get { return height; }
+    }
+
+    public double Weight
+    {
+        get { return weight; }
+    }
+
+    public double GetVolume()
+    {
+        return length * height * weight;
+    }
+}
